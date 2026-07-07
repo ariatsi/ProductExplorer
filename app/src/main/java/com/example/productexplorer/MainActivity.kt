be2c9892a7613e6841d7d7b3cfc340d4bd564f42
ariatsi <92860360+ariatsi@users.noreply.github.com>
2026-07-07 18:33:00 +0200

    Version finale TP7 : Personnalisation graphique (Material Design 3 et thème)

--- app/src/main/java/com/example/productexplorer/MainActivity.kt
+++ app/src/main/java/com/example/productexplorer/MainActivity.kt
@@ -38,8 +38,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.productexplorer.ui.theme.ProductExplorerTheme
 
-import androidx.compose.material3.CardDefaults
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
